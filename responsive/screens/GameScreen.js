868088e7fb819be5bd8ee64bb6f74524c1add194
Marco Santarcangelo Zazzetta <marcosantar93@gmail.com>
2019-11-01 09:25:58 -0300

    dimensions apo

--- responsive/screens/GameScreen.js
+++ responsive/screens/GameScreen.js
@@ -1,19 +1,20 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 import {
   View,
   Text,
   StyleSheet,
   Alert,
   ScrollView,
-  FlatList
-} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+  FlatList,
+  Dimensions
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-import NumberContainer from '../components/NumberContainer';
-import Card from '../components/Card';
-import MainButton from '../components/MainButton';
-import BodyText from '../components/BodyText';
-import DefaultStyles from '../constants/default-styles';
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+import MainButton from "../components/MainButton";
+import BodyText from "../components/BodyText";
+import DefaultStyles from "../constants/default-styles";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -50,15 +51,15 @@ const GameScreen = props => {
 
   const nextGuessHandler = direction => {
     if (
-      (direction === 'lower' && currentGuess < props.userChoice) ||
-      (direction === 'greater' && currentGuess > props.userChoice)
+      (direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)
     ) {
-      Alert.alert("Don't lie!", 'You know that this is wrong...', [
-        { text: 'Sorry!', style: 'cancel' }
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" }
       ]);
       return;
     }
-    if (direction === 'lower') {
+    if (direction === "lower") {
       currentHigh.current = currentGuess;
     } else {
       currentLow.current = currentGuess + 1;
@@ -76,19 +77,24 @@ const GameScreen = props => {
     ]);
   };
 
+  let listContainerStyle = styles.listContainer;
+
+  if (Dimensions.get("window").width < 350)
+    listContainerStyle = styles.listContainerBig;
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+        <MainButton onPress={nextGuessHandler.bind(this, "lower")}>
           <Ionicons name="md-remove" size={24} color="white" />
         </MainButton>
-        <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+        <MainButton onPress={nextGuessHandler.bind(this, "greater")}>
           <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
-      <View style={styles.listContainer}>
+      <View style={listContainerStyle}>
         {/* <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
         </ScrollView> */}
@@ -107,33 +113,37 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center'
+    alignItems: "center"
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
-    maxWidth: '90%'
+    maxWidth: "90%"
   },
   listContainer: {
     flex: 1,
-    width: '60%'
+    width: "60%"
+  },
+  listContainerBig: {
+    flex: 1,
+    width: "80%"
   },
   list: {
     flexGrow: 1,
     // alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end"
   },
   listItem: {
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
     padding: 15,
     marginVertical: 10,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%'
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%"
   }
 });
 
--- responsive/screens/StartGameScreen.js
+++ responsive/screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -6,28 +6,29 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
-  Alert
-} from 'react-native';
+  Alert,
+  Dimensions
+} from "react-native";
 
-import Card from '../components/Card';
-import Input from '../components/Input';
-import NumberContainer from '../components/NumberContainer';
-import BodyText from '../components/BodyText';
-import TitleText from '../components/TitleText';
-import MainButton from '../components/MainButton';
-import Colors from '../constants/colors';
+import Card from "../components/Card";
+import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+import MainButton from "../components/MainButton";
+import Colors from "../constants/colors";
 
 const StartGameScreen = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState("");
   const [confirmed, setConfirmed] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState();
 
   const numberInputHandler = inputText => {
-    setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
   };
 
   const resetInputHandler = () => {
-    setEnteredValue('');
+    setEnteredValue("");
     setConfirmed(false);
   };
 
@@ -35,15 +36,15 @@ const StartGameScreen = props => {
     const chosenNumber = parseInt(enteredValue);
     if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
       Alert.alert(
-        'Invalid number!',
-        'Number has to be a number between 1 and 99.',
-        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
       );
       return;
     }
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
-    setEnteredValue('');
+    setEnteredValue("");
     Keyboard.dismiss();
   };
 
@@ -108,35 +109,38 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center'
+    alignItems: "center"
   },
   title: {
     fontSize: 20,
     marginVertical: 10,
-    fontFamily: 'open-sans-bold'
+    fontFamily: "open-sans-bold"
   },
   inputContainer: {
+    width: "80%",
     minWidth: 300,
-    width: '80%',
-    maxWidth: "95%",
-    alignItems: 'center'
+    maxWidth: "85%",
+    alignItems: "center"
   },
   buttonContainer: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
     paddingHorizontal: 15
   },
   button: {
-    width: 100
+    width: Dimensions.get("window").width / 4
+    // dimension lets you check the device's dimensions
+    // if we used percentages, it would use the parent component dimensions
+    // window doesn't take the status bar into account on android
   },
   input: {
     width: 50,
-    textAlign: 'center'
+    textAlign: "center"
   },
   summaryContainer: {
     marginTop: 20,
-    alignItems: 'center'
+    alignItems: "center"
   }
 });
 
