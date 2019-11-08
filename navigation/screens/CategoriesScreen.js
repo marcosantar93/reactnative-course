507cc81c0e7efbe1addc160ef065475baab7432f
Marco Santarcangelo <marcosantar93@gmail.com>
2019-11-07 21:42:17 -0300

    navigation with parameters

--- navigation/screens/CategoriesScreen.js
+++ navigation/screens/CategoriesScreen.js
@@ -1,24 +1,48 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Platform
+} from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoriesScreen = props => {
+  const renderGridItem = itemData => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate({ routeName: "CategoryMeals", params: { categoryId: itemData.item.id } });
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
-    <View style={styles.screen}>
-      <Text>The CategoriesScreen!</Text>
-      <Button
-        title="Go to Meals!"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals" });
-        }}
-      />
-      <Button
-        title="Go to Meals! Can't go back!"
-        onPress={() => {
-          props.navigation.replace("CategoryMeals");
-        }}
-      />
-    </View>
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    ></FlatList>
   );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ""
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
 };
 
 const styles = StyleSheet.create({
@@ -26,6 +50,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 
--- navigation/screens/CategoryMealsScreen.js
+++ navigation/screens/CategoryMealsScreen.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Platform } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoryMealsScreen = props => {
+  const catId = props.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
   return (
     <View style={styles.screen}>
       <Text>The CategoryMealsScreen!</Text>
@@ -25,6 +32,20 @@ const CategoryMealsScreen = props => {
       />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam("categoryId");
+  
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ""
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
+  };
 };
 
 const styles = StyleSheet.create({
